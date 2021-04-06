42d697a4b51044cf015ddbb91df941c9841cf6d3
Popov Denis <denpopov.2000@mail.ru>
2021-04-06 22:55:16 +0300

    Add comments and call class

--- src/main/java/ListCompare.java
+++ src/main/java/ListCompare.java
@@ -1,19 +1,36 @@
 import java.util.List;
-
+/**
+ * Класс - коллекция позволяющий сравнивать производительность наследников List
+ * @author Denis Popov
+ * @version 1.0
+ * @param <T> тип элемента Списка
+ */
 public class ListCompare<T> {
     List<T> list;
     public ListCompare(List<T> enterList){
         list=enterList;
     }
+    /**
+     *  Проверка всех методов
+     * @param count - принимает количество элементов
+     * @param value - принимает элемент для вставки
+     * @return - возвращает массив с временем работы каждого метода
+     */
     public long[] compareAll(int count, T value){
         long[] testList= new long[3];
         testList[0]=compareAdd(count,value);
-        testList[1]=compareDelete(count,value);
-        testList[2]=compareGet(count,value);
+        testList[1]=compareGet(count,value);
+        testList[2]=compareDelete(count,value);
         return testList;
     }
+    /**
+     *  Проверка метода вставки
+     * @param count - принимает количество элементов
+     * @param value - принимает элемент для вставки
+     * @return - возвращает время работы метода
+     */
     public long compareAdd(int count, T value){
-        list.clear();
+        list.clear(); //Очишаем список на случай уже имеющихся данных
         long time = System.nanoTime();
         for (int i=0;i<count;i++){
             list.add(value);
@@ -21,19 +38,35 @@ public class ListCompare<T> {
         time=System.nanoTime() - time;
         return time;
     }
+    /**
+     *  Проверка метода удаления элементов
+     * @param count - принимает количество элементов
+     * @param value - принимает элемент для вставки
+     * @return - возвращает время работы метода
+     */
     public long compareDelete(int count, T value){
+        /* Для того что бы была возможность тестирования каждого метода по отдельности
+        * инициализирум каждый раз метод нужным для тестирования количеством элементов*/
         compareAdd(count,value);
         long time = System.nanoTime();
         for (int i=count-1;i>=count/2;i--){
             list.remove(i);
         }
         for (int i=0;i<count/2;i++){
-            list.remove(i);
+            list.remove(0);
         }
         time=System.nanoTime() - time;
         return time;
     }
+    /**
+     *  Проверка метода выбора элементов
+     * @param count - принимает количество элементов
+     * @param value - принимает элемент для вставки
+     * @return - возвращает время работы метода
+     */
     public long compareGet(int count, T value){
+        /* Для того что бы была возможность тестирования каждого метода по отдельности
+         * инициализирум каждый раз метод нужным для тестирования количеством элементов*/
         compareAdd(count,value);
         long time = System.nanoTime();
         for (int i=count/2;i<count;i++){
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,23 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+/**
+ * @author Denis Popov
+ * @version 1.0
+ */
 public class Main {
     public static void main(String[] args) {
-        ListCompare listCompare=new ListCompare();
+        ArrayList<Integer> arrayList =new ArrayList<Integer>();
+        LinkedList<Integer> linkedList =  new LinkedList<Integer>();
+        ListCompare listCompareAL=new ListCompare(arrayList);
+        ListCompare listCompareLL=new ListCompare(linkedList);
+
+        /*Цикл тестирования разного количества шагов*/
         for (int i=100;i<=100000;i*=10)
         {
-            System.out.println(listCompare.compareDelete(i));
-            System.out.println(listCompare.compareAdd(i));
+            long[] arrayResult=listCompareAL.compareAll(i,5);
+            long[] listResult=listCompareLL.compareAll(i,5);
+            System.out.println("ArrayList ("+i + " count) " +arrayResult[0] + "ms -Add " +arrayResult[1] + "ms - Get " +arrayResult[2] + "ms - Delete "  );
+            System.out.println("LinkedList ("+i + " count) " +listResult[0] + "ms -Add " +listResult[1] + "ms - Get " +listResult[2] + "ms - Delete \n"  );
         }
 
     }
